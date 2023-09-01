03f9b8e886c5d49fa5c7339540cdb9a56c44a743
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2023-09-01 08:28:35 -0400

    Register page

--- components-builder/src/main.tsx
+++ components-builder/src/main.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-import Login from "./components/Login/Login";
+import Register from "./pages/Register/Register";
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<Login />
+		<Register />
 	</React.StrictMode>,
 );
 
