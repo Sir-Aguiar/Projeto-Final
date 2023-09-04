c40a670efdae68b137bf82e0dd22fa368680601a
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2023-09-04 10:35:31 -0400

    School page (Table and Controller secs)

--- components-builder/src/main.tsx
+++ components-builder/src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import "./index.css";
 import Escolas from "./pages/Escolas/Escolas";
 ReactDOM.createRoot(document.getElementById("root")!).render(
