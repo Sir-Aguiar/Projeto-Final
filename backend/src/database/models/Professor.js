63d58cb53e13fcd3e1400ad316c90b3f852ad2b9
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2023-09-04 08:00:03 -0400

    Fixing field in table 'professor'

--- backend/src/database/models/Professor.js
+++ backend/src/database/models/Professor.js
@@ -1,30 +1,35 @@
 const { DataTypes } = require("sequelize");
 const Database = require("../database");
 const Professor = Database.define(
-  "Professor",
-  {
-    idProfessor: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    nome: {
-      type: DataTypes.STRING(150),
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-      unique: true,
-    },
-    senha: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-  },
-  {
-    tableName: "professores",
-  },
+	"Professor",
+	{
+		idProfessor: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		nome: {
+			type: DataTypes.STRING(150),
+			allowNull: false,
+		},
+		email: {
+			type: DataTypes.STRING(255),
+			allowNull: false,
+			unique: true,
+		},
+		senha: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+		login: {
+			type: DataTypes.STRING(30),
+			unique: true,
+			allowNull: false,
+		},
+	},
+	{
+		tableName: "professores",
+	},
 );
 
 module.exports = Professor;
