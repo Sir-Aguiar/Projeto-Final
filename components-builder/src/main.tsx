8ab8a3810357d0518ae7b5219be920b4d2ad55dd
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2023-09-04 09:32:00 -0400

    New dependencies

--- components-builder/src/main.tsx
+++ components-builder/src/main.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-import LandingPage from "./pages/LandingPage/LandingPage";
+import Escolas from "./pages/Escolas/Escolas";
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <LandingPage />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<Escolas />
+	</React.StrictMode>,
 );
---
+++ components-builder/src/pages/Escolas/Escolas.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Escolas.module.css";
+
+const Escolas: React.FC = () => {
+	return (
+		<div className={styles.content_container}>
+			<div className={styles.controllers}>
+				<header className={styles.actions}>
+        </header>
+			</div>
+		</div>
+	);
+};
+
+export default Escolas;
