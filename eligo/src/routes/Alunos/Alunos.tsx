d0d805de3f68198ea2e943d1f38ef8ec90b2a5ba
FELIPE_AGUIAR <felipeferreiraaguiar9@gmail.com>
2023-09-22 06:30:39 -0400

    Students Loading

--- eligo/src/routes/Alunos/Alunos.tsx
+++ eligo/src/routes/Alunos/Alunos.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styles from "./Alunos.module.css";
-import { Checkbox, Divider, Typography } from "@mui/material";
+import { Checkbox, Divider, Typography, CircularProgress } from "@mui/material";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import LibraryAddIcon from "@mui/icons-material/LibraryAdd";
 import EditIcon from "@mui/icons-material/Edit";
@@ -13,124 +13,129 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
 const Alunos: React.FC = () => {
-	const {
-		Alunos,
-		AlunosQTD,
-		selectRow,
-		selectedRows,
-		loadMore,
-		DrawerCreate,
-		ModalDelete,
-		DrawerUpdate,
-		EscolasState,
-		TurmasState,
-		showStudent,
-		showClasses,
-	} = useAlunosContext();
-	const [selectedIdEscola, setSelectedIdEscola] = useState("");
-	const [selectedIdTurma, setSelectedIdTurma] = useState("");
-	return (
-		<div className={styles.content_container}>
-			<div className={styles.controllers}>
-				<header className={styles.actions}>
-					<button title="Cadastrar aluno" onClick={() => DrawerCreate.open()}>
-						<LibraryAddIcon />
-					</button>
-					<button disabled={selectedRows.length !== 1} title="Editar alunos" onClick={() => DrawerUpdate.open()}>
-						<EditIcon />
-					</button>
-					<button disabled={selectedRows.length < 1} title="Excluir alunos" onClick={() => ModalDelete.open()}>
-						<DeleteForeverIcon />
-					</button>
-				</header>
+  const {
+    Alunos,
+    AlunosQTD,
+    selectRow,
+    selectedRows,
+    loadMore,
+    DrawerCreate,
+    ModalDelete,
+    DrawerUpdate,
+    EscolasState,
+    TurmasState,
+    showStudent,
+    isLoading,
+    showClasses,
+  } = useAlunosContext();
+  const [selectedIdEscola, setSelectedIdEscola] = useState("");
+  const [selectedIdTurma, setSelectedIdTurma] = useState("");
+  return (
+    <div className={styles.content_container}>
+      <div className={styles.controllers}>
+        <header className={styles.actions}>
+          <button title="Cadastrar aluno" onClick={() => DrawerCreate.open()}>
+            <LibraryAddIcon />
+          </button>
+          <button disabled={selectedRows.length !== 1} title="Editar alunos" onClick={() => DrawerUpdate.open()}>
+            <EditIcon />
+          </button>
+          <button disabled={selectedRows.length < 1} title="Excluir alunos" onClick={() => ModalDelete.open()}>
+            <DeleteForeverIcon />
+          </button>
+        </header>
 
-				<div className={styles.classes}>
-					<FormControl fullWidth>
-						<InputLabel>Escola</InputLabel>
-						<Select
-							value={selectedIdEscola}
-							required
-							label="Escola"
-							onChange={(e: any) => {
-								setSelectedIdEscola(e.target.value);
-								showStudent(Number(e.target.value));
-								showClasses(Number(e.target.value));
-							}}
-						>
-							{EscolasState.length > 0 &&
-								EscolasState.map((escola, index) => (
-									<MenuItem key={index} value={escola.idEscola}>
-										{escola.nome}
-									</MenuItem>
-								))}
-						</Select>
-					</FormControl>
-					<FormControl fullWidth disabled={TurmasState.length < 1}>
-						<InputLabel>Turma</InputLabel>
-						<Select
-							value={selectedIdTurma}
-							required
-							label="Turma"
-							onChange={(e: any) => {
-								setSelectedIdTurma(e.target.value);
-								showStudent(Number(e.target.value));
-							}}
-						>
-							{TurmasState.length > 0 &&
-								TurmasState.map((turma, index) => (
-									<MenuItem key={index} value={turma.idTurma}>
-										{turma.nome}
-									</MenuItem>
-								))}
-						</Select>
-					</FormControl>
-				</div>
-			</div>
-			<div className={styles.table_container}>
-				<table className={styles.content_table}>
-					<thead className={styles.table_header}>
-						<tr>
-							<th className="min-w-[50px] w-[50px] max-w-[50px]"></th>
-							<th className="min-w-[150px] w-[200px]">Nome</th>
-							<th className="min-w-[100px] w-[100px]">Turma</th>
-							<th className="min-w-[125px] w-[125px]">Série</th>
-							<th className="min-w-[200px] w-[250px]">Escola</th>
-						</tr>
-					</thead>
-					<tbody className={styles.table_body}>
-						{Alunos.length > 0 &&
-							Alunos.map((aluno, index) => (
-								<tr key={index}>
-									<td>
-										<Checkbox
-											checked={selectedRows.includes(aluno.idAluno)}
-											onChange={() => selectRow(aluno.idAluno)}
-										/>
-									</td>
-									<td>{aluno.nome}</td>
-									<td>{aluno.turma.nome}</td>
-									<td>{aluno.turma.curso.nome}</td>
-									<td>{aluno.escola.nome}</td>
-								</tr>
-							))}
-						{Alunos.length < AlunosQTD && (
-							<tr onClick={loadMore}>
-								<td
-									colSpan={5}
-									className="text-center cursor-pointer underline transition-all duration-200 hover:text-blue-600 "
-								>
-									<span className="text-[16px] font-bold">Carregar mais</span>
-								</td>
-							</tr>
-						)}
-					</tbody>
-				</table>
-			</div>
-			<Create />
-			<Update />
-			<Delete />
-		</div>
-	);
+        <div className={styles.classes}>
+          <FormControl fullWidth>
+            <InputLabel>Escola</InputLabel>
+            <Select
+              value={selectedIdEscola}
+              required
+              label="Escola"
+              onChange={(e: any) => {
+                setSelectedIdEscola(e.target.value);
+                showStudent(Number(e.target.value));
+                showClasses(Number(e.target.value));
+              }}
+            >
+              {EscolasState.length > 0 &&
+                EscolasState.map((escola, index) => (
+                  <MenuItem key={index} value={escola.idEscola}>
+                    {escola.nome}
+                  </MenuItem>
+                ))}
+            </Select>
+          </FormControl>
+          <FormControl fullWidth disabled={TurmasState.length < 1}>
+            <InputLabel>Turma</InputLabel>
+            <Select
+              value={selectedIdTurma}
+              required
+              label="Turma"
+              onChange={(e: any) => {
+                setSelectedIdTurma(e.target.value);
+                showStudent(Number(e.target.value));
+              }}
+            >
+              {TurmasState.length > 0 &&
+                TurmasState.map((turma, index) => (
+                  <MenuItem key={index} value={turma.idTurma}>
+                    {turma.nome}
+                  </MenuItem>
+                ))}
+            </Select>
+          </FormControl>
+        </div>
+      </div>
+      <div className={styles.table_container}>
+        {isLoading ? (
+          <div className="w-full h-[50px] flex justify-center items-center">
+            <CircularProgress />
+          </div>
+        ) : Alunos.length < 1 ? (
+          <div className="w-full h-[50px] text-center">
+            <h1>Nenhum aluno foi encontrado</h1>
+          </div>
+        ) : (
+          <table className={styles.content_table}>
+            <thead className={styles.table_header}>
+              <tr>
+                <th className="min-w-[50px] w-[50px] max-w-[50px]"></th>
+                <th className="min-w-[150px] w-[200px]">Nome</th>
+                <th className="min-w-[100px] w-[100px]">Turma</th>
+                <th className="min-w-[125px] w-[125px]">Série</th>
+                <th className="min-w-[200px] w-[250px]">Escola</th>
+              </tr>
+            </thead>
+            <tbody className={styles.table_body}>
+              {isLoading && (
+                <tr>
+                  <td colSpan={6}></td>
+                </tr>
+              )}
+              {Alunos.map((aluno, index) => (
+                <tr key={index}>
+                  <td>
+                    <Checkbox
+                      checked={selectedRows.includes(aluno.idAluno)}
+                      onChange={() => selectRow(aluno.idAluno)}
+                    />
+                  </td>
+                  <td>{aluno.nome}</td>
+                  <td>{aluno.turma.nome}</td>
+                  <td>{aluno.turma.curso.nome}</td>
+                  <td>{aluno.escola.nome}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+      <Create />
+      <Update />
+      <Delete />
+    </div>
+  );
 };
 
 export default Alunos;
--- eligo/src/routes/Alunos/RouteStateManager.tsx
+++ eligo/src/routes/Alunos/RouteStateManager.tsx
@@ -3,203 +3,208 @@ import React, { createContext, useState, useMemo, useContext, useEffect } from "
 import { useAuthHeader } from "react-auth-kit";
 
 type ProviderProps = {
-	children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 interface IModalProps {
-	situation: boolean;
-	open: () => void;
-	close: () => void;
+  situation: boolean;
+  open: () => void;
+  close: () => void;
 }
 
 interface ITurma {
-	idTurma: number;
-	idCurso: number;
-	idEscola: number;
-	nome: string;
-	escola: {
-		idGestor: number;
-		nome: string;
-	};
-	curso: {
-		nome: string;
-	};
+  idTurma: number;
+  idCurso: number;
+  idEscola: number;
+  nome: string;
+  escola: {
+    idGestor: number;
+    nome: string;
+  };
+  curso: {
+    nome: string;
+  };
 }
 interface IEscola {
-	idEscola: number;
-	idGestor: number;
-	nome: string;
+  idEscola: number;
+  idGestor: number;
+  nome: string;
 }
 
 interface IAluno {
-	idAluno: number;
-	nome: string;
-	escola: {
-		idEscola: number;
-		nome: string;
-	};
-	turma: {
-		idTurma: number;
-		nome: string;
-		curso: {
-			idCurso: number;
-			nome: string;
-		};
-	};
+  idAluno: number;
+  nome: string;
+  escola: {
+    idEscola: number;
+    nome: string;
+  };
+  turma: {
+    idTurma: number;
+    nome: string;
+    curso: {
+      idCurso: number;
+      nome: string;
+    };
+  };
 }
 
 interface IRouteContext {
-	Alunos: IAluno[];
-	EscolasState: IEscola[];
-	TurmasState: ITurma[];
-	AlunosQTD: number;
-	RouteAPI: AxiosInstance;
-	DrawerCreate: IModalProps;
-	DrawerUpdate: IModalProps;
-	ModalDelete: IModalProps;
-	selectedRows: number[];
-	selectRow: (idTurma: number) => void;
-	loadMore: () => Promise<void>;
-	showSchools: () => Promise<void>;
-	showClasses: (idEscola?: number) => Promise<void>;
-	showStudent: (idEscola?: number) => Promise<void>;
+  Alunos: IAluno[];
+  EscolasState: IEscola[];
+  TurmasState: ITurma[];
+  AlunosQTD: number;
+  RouteAPI: AxiosInstance;
+  DrawerCreate: IModalProps;
+  DrawerUpdate: IModalProps;
+  ModalDelete: IModalProps;
+  selectedRows: number[];
+  selectRow: (idTurma: number) => void;
+  loadMore: () => Promise<void>;
+  showSchools: () => Promise<void>;
+  showClasses: (idEscola?: number) => Promise<void>;
+  showStudent: (idEscola?: number) => Promise<void>;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  isLoading: boolean;
 }
 
 const RouteContext = createContext<IRouteContext | null>(null);
 
 const AlunosProvider: React.FC<ProviderProps> = ({ children }) => {
-	const authHeader = useAuthHeader();
-	const [isCreateOpen, setCreateDrawer] = useState(false);
-	const [isUpdateOpen, setUpdateDrawer] = useState(false);
-	const [isDeleteOpen, setDeleteModal] = useState(false);
-	const [selectedRows, setRows] = useState<number[]>([]);
-	const [Alunos, setAlunos] = useState<IAluno[]>([]);
-	const [AlunosQTD, setAlunosQTD] = useState(0);
-	const [TurmasState, setTurmas] = useState<ITurma[]>([]);
-	const [EscolasState, setEscolas] = useState<IEscola[]>([]);
+  const authHeader = useAuthHeader();
+  const [isCreateOpen, setCreateDrawer] = useState(false);
+  const [isUpdateOpen, setUpdateDrawer] = useState(false);
+  const [isDeleteOpen, setDeleteModal] = useState(false);
+  const [selectedRows, setRows] = useState<number[]>([]);
+  const [Alunos, setAlunos] = useState<IAluno[]>([]);
+  const [AlunosQTD, setAlunosQTD] = useState(0);
+  const [TurmasState, setTurmas] = useState<ITurma[]>([]);
+  const [EscolasState, setEscolas] = useState<IEscola[]>([]);
+  const [isLoading, setLoading] = useState(false);
+  // Axios instance
+  const RouteAPI = axios.create({
+    baseURL: import.meta.env.VITE_SERVER_URL,
+    headers: {
+      Authorization: authHeader(),
+    },
+  });
 
-	// Axios instance
-	const RouteAPI = axios.create({
-		baseURL: import.meta.env.VITE_SERVER_URL,
-		headers: {
-			Authorization: authHeader(),
-		},
-	});
+  const DrawerCreate: IModalProps = useMemo(() => {
+    return {
+      situation: isCreateOpen,
+      open: () => {
+        showSchools().then(() => setCreateDrawer(true));
+      },
+      close: () => {
+        showStudent().then(() => setCreateDrawer(false));
+        setEscolas([]);
+        setTurmas([]);
+      },
+    };
+  }, [isCreateOpen]);
 
-	const DrawerCreate: IModalProps = useMemo(() => {
-		return {
-			situation: isCreateOpen,
-			open: () => {
-				showSchools().then(() => setCreateDrawer(true));
-			},
-			close: () => {
-				showStudent().then(() => setCreateDrawer(false));
-				setEscolas([]);
-				setTurmas([]);
-			},
-		};
-	}, [isCreateOpen]);
+  const DrawerUpdate: IModalProps = useMemo(() => {
+    return {
+      situation: isUpdateOpen,
+      open: () => {
+        showClasses().then(() => setUpdateDrawer(true));
+      },
+      close: () => {},
+    };
+  }, [isUpdateOpen]);
 
-	const DrawerUpdate: IModalProps = useMemo(() => {
-		return {
-			situation: isUpdateOpen,
-			open: () => {
-				showClasses().then(() => setUpdateDrawer(true));
-			},
-			close: () => {},
-		};
-	}, [isUpdateOpen]);
+  const ModalDelete: IModalProps = useMemo(() => {
+    return {
+      situation: isDeleteOpen,
+      open: () => setDeleteModal(true),
+      close: () => {
+        showStudent().then(() => {
+          setDeleteModal(false);
+          setRows([]);
+        });
+      },
+    };
+  }, [isDeleteOpen]);
 
-	const ModalDelete: IModalProps = useMemo(() => {
-		return {
-			situation: isDeleteOpen,
-			open: () => setDeleteModal(true),
-			close: () => {
-				showStudent().then(() => {
-					setDeleteModal(false);
-					setRows([]);
-				});
-			},
-		};
-	}, [isDeleteOpen]);
+  const selectRow = (idAluno: number) => {
+    setRows((values) => {
+      let newValues: number[];
 
-	const selectRow = (idAluno: number) => {
-		setRows((values) => {
-			let newValues: number[];
-
-			if (values.includes(idAluno)) {
-				newValues = values.filter((value) => value !== idAluno);
-			} else {
-				newValues = [...values, idAluno];
-			}
-			/* 			if (newValues.length === 1) {
+      if (values.includes(idAluno)) {
+        newValues = values.filter((value) => value !== idAluno);
+      } else {
+        newValues = [...values, idAluno];
+      }
+      /* 			if (newValues.length === 1) {
 				showClassStudents(newValues[0]);
 			} else {
 				setAlunoTurma([]);
 			} */
-			return newValues;
-		});
-	};
+      return newValues;
+    });
+  };
 
-	const showSchools = async () => {
-		const response = await RouteAPI.get("/escola");
-		setEscolas(response.data.escolas);
-	};
+  const showSchools = async () => {
+    const response = await RouteAPI.get("/escola");
+    setEscolas(response.data.escolas);
+  };
 
-	const showClasses = async (idEscola?: number) => {
-		const response = await RouteAPI.get(`/turma${idEscola && `?idEscola=${idEscola}`}`);
-		setTurmas(response.data.turmas);
-	};
+  const showClasses = async (idEscola?: number) => {
+    const response = await RouteAPI.get(`/turma${idEscola && `?idEscola=${idEscola}`}`);
+    setTurmas(response.data.turmas);
+  };
 
-	const showStudent = async () => {
-		const response = await RouteAPI.get(`/aluno`);
-		console.log(response.data.alunos);
-		setAlunos(response.data.alunos);
-		setAlunosQTD(response.data.qtd);
-	};
+  const showStudent = async () => {
+    setLoading(true);
+    const response = await RouteAPI.get(`/aluno`);
+    setAlunos(response.data.alunos);
+    setAlunosQTD(response.data.qtd);
+    setLoading(false);
+  };
 
-	const loadMore = async () => {
-		const skip = Alunos.length;
-		const response = await RouteAPI.get(`/aluno?skip=${skip}`);
-		setAlunos((values) => [...values, ...response.data.alunos]);
-		setAlunosQTD(response.data.qtd);
-	};
+  const loadMore = async () => {
+    const skip = Alunos.length;
+    const response = await RouteAPI.get(`/aluno?skip=${skip}`);
+    setAlunos((values) => [...values, ...response.data.alunos]);
+    setAlunosQTD(response.data.qtd);
+  };
 
-	useEffect(() => {
-		showStudent();
-		showSchools();
-	}, []);
+  useEffect(() => {
+    showStudent();
+    showSchools();
+  }, []);
 
-	return (
-		<RouteContext.Provider
-			value={{
-				loadMore,
-				Alunos,
-				AlunosQTD,
-				DrawerCreate,
-				DrawerUpdate,
-				ModalDelete,
-				RouteAPI,
-				selectedRows,
-				EscolasState,
-				TurmasState,
-				selectRow,
-				showClasses,
-				showSchools,
-				showStudent,
-			}}
-		>
-			{children}
-		</RouteContext.Provider>
-	);
+  return (
+    <RouteContext.Provider
+      value={{
+        loadMore,
+        Alunos,
+        AlunosQTD,
+        DrawerCreate,
+        DrawerUpdate,
+        ModalDelete,
+        RouteAPI,
+        selectedRows,
+        EscolasState,
+        TurmasState,
+        selectRow,
+        showClasses,
+        showSchools,
+        showStudent,
+        isLoading,
+        setLoading,
+      }}
+    >
+      {children}
+    </RouteContext.Provider>
+  );
 };
 
 const useAlunosContext = () => {
-	const context = useContext(RouteContext);
+  const context = useContext(RouteContext);
 
-	if (!context) throw new Error("AlunosContext deve ser chamado dentro de AlunosProvider");
+  if (!context) throw new Error("AlunosContext deve ser chamado dentro de AlunosProvider");
 
-	return context;
+  return context;
 };
 
 export { AlunosProvider, useAlunosContext };
