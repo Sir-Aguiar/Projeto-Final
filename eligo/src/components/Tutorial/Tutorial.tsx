6413f3367d7dc6bf35b4aa24d92f65c014f3df8d
FELIPE_AGUIAR <felipeferreiraaguiar9@gmail.com>
2023-10-06 13:50:46 -0400

    Homepage help

---
+++ eligo/src/components/Tutorial/Tutorial.tsx
@@ -0,0 +1,40 @@
+import { ArrowRight } from "@mui/icons-material";
+import { Divider } from "@mui/material";
+
+const TutorialTitle: React.FC<{ children: string }> = ({ children }) => {
+  return (
+    <Divider flexItem className="font-Montserrat font-semibold">
+      {children}
+    </Divider>
+  );
+};
+
+const TutorialStep: React.FC<{ title: string; stage?: number; children: JSX.Element[] | JSX.Element }> = ({
+  title,
+  children,
+  stage,
+}) => {
+  return (
+    <div className="w-full flex flex-col gap-3">
+      <div className="w-full flex items-center justify-start gap-1">
+        <div className="flex items-center">
+          <div className="w-[25px] h-[25px] flex items-center justify-center rounded-full bg-blue-500 text-background text-sm font-Lato font-bold">
+            {stage}
+          </div>
+          <div className="w-[25px] h-[2px] rounded-full bg-blue-500"></div>
+        </div>
+        <h1 className="text-blue-500 text-sm font-medium">{title}</h1>
+      </div>
+      <div className="flex items-center w-full gap-2">
+        <ArrowRight />
+        <div className="w-full text-[12px] flex flex-col gap-2">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+const Tutorial: React.FC<{ area: 2 | 3; children: JSX.Element[] }> = ({ area, children }) => {
+  return <div className={`flex flex-col items-center justify-start gap-3 col-span-${area} mobile:col-span-1`}>{children}</div>;
+};
+
+export { Tutorial, TutorialStep, TutorialTitle };
--- eligo/src/routes/Homepage/Homepage.tsx
+++ eligo/src/routes/Homepage/Homepage.tsx
@@ -5,9 +5,11 @@ import ClassroomBro from "../../assets/Homepage/Classroom-bro.png";
 import LearningCuate from "../../assets/Homepage/Learning-cuate.png";
 import ModernWoman from "../../assets/Homepage/Modern woman.gif";
 import { Link } from "react-router-dom";
-import { AdminPanelSettings, ArrowForward, ArrowForwardIos, ArrowRightTwoTone } from "@mui/icons-material";
+import { AdminPanelSettings, ArrowForward, ArrowForwardIos, ArrowRight, ArrowRightTwoTone } from "@mui/icons-material";
 import AutoStoriesIcon from "@mui/icons-material/AutoStories";
 import UpcomingIcon from "@mui/icons-material/Upcoming";
+import { Divider } from "@mui/material";
+import { Tutorial, TutorialStep, TutorialTitle } from "../../components/Tutorial/Tutorial";
 const Homepage: React.FC = () => {
   return (
     <div className={styles.content_section}>
@@ -61,6 +63,99 @@ const Homepage: React.FC = () => {
           <img src={LearningCuate} className="max-h-[150px]" />
         </div>
       </header>
+      <main className={styles.home_body}>
+        <section className={styles.tutorial_section}>
+          <Tutorial area={2}>
+            <TutorialTitle>Como iniciar aula</TutorialTitle>
+            <TutorialStep title="Permissões">
+              <p>Como gestor, poderá iniciar aulas de qualquer disciplina na grade da turma.</p>
+              <p>Como professor, somente das disciplinas que lhe foi concedido acesso.</p>
+            </TutorialStep>
+            <TutorialStep title="Vá até a página de aulas">
+              <p>Informe a turma e disciplina. </p>
+              <p>Clique em "Iniciar Aula".</p>
+            </TutorialStep>
+          </Tutorial>
+          <Tutorial area={3}>
+            <TutorialTitle>Passos Iniciais</TutorialTitle>
+            <TutorialStep title="Cadastre uma escola" stage={1}>
+              <p>Na página “escolas”, clique em “Adicionar”.</p>
+              <p>Ao cadastrar uma escola, será considerado seu gestor.</p>
+            </TutorialStep>
+            <TutorialStep title="Insira turmas" stage={2}>
+              <p>Na página “turmas”, clique em “Adicionar”.</p>
+              <p>
+                Selecione a escola na qual deseja inserir as turmas. Esta ação é válida apenas as escolas em que é
+                gestor.
+              </p>
+            </TutorialStep>
+            <TutorialStep title="Matricule alunos" stage={3}>
+              <p>Na página “alunos”, clique em “Adicionar”.</p>
+              <p>Selecione a turma na qual matriculará o aluno. Novamente, esta ação é acessível apenas ao gestor.</p>
+            </TutorialStep>
+          </Tutorial>
+          <Tutorial area={2}>
+            <TutorialTitle>Como inserir disciplinas</TutorialTitle>
+            <TutorialStep title="Vá até a página da escola">
+              <p>
+                Na página “escolas”, clique no ícone na frente do nome. Esta página é acessível apenas ao gestor da
+                escola.
+              </p>
+            </TutorialStep>
+            <TutorialStep title="Clique em “Adicionar Disciplina”">
+              <p>Insira o nome e o curso (série) ao qual esta disciplina será associada (grade).</p>
+              <p>
+                Estas informações podem ser alteradas posteriormente ao clicar no ícone em “Disciplinas ministradas”.
+              </p>
+            </TutorialStep>
+          </Tutorial>
+        </section>
+        <section className={styles.tutorial_section}>
+          <Tutorial area={2}>
+            <TutorialTitle>Como vincular professores</TutorialTitle>
+            <TutorialStep title="Vá até a página da escola">
+              <p>Na página “escolas”, clique no ícone na frente do nome. </p>
+            </TutorialStep>
+            <TutorialStep title="Clique em “Adicionar Professor”">
+              <p>Insira o email cadastrado pelo usuário que deseja associar.</p>
+              <p>
+                Informe uma turma a qual este usuário será associado (só poderá ser associado com as as disciplinas na
+                grade da turma).
+              </p>
+              <p>Clique em “Cadastrar” e este usuário já possui acesso à turma informada.</p>
+            </TutorialStep>
+          </Tutorial>
+          <Tutorial area={3}>
+            <TutorialTitle>Paineis Analíticos</TutorialTitle>
+            <TutorialStep title="Turma" stage={1}>
+              <p>Ao acessar à página da turma terá acesso a:</p>
+              <p>Um gráfico comparando a quantidade de alunos na turma, e alunos matriculados no mesmo curso.</p>
+              <p>Os dados da turma em questão, como alunos, suas faltas, etc...</p>
+              <p>Um gráfico exibindo a média de faltas em cada aula por mês no decorrer do ano.</p>
+            </TutorialStep>
+            <TutorialStep title="Aluno" stage={2}>
+              <p>Ao acesso à página do aluno terá acesso a:</p>
+              <p>Quantidade de faltas do aluno no decorrer do ano.</p>
+              <p>Comparação da média de faltas do curso, com as faltas deste aluno.</p>
+              <p>E a distribuição das faltas por disciplina.</p>
+            </TutorialStep>
+          </Tutorial>
+          <Tutorial area={2}>
+            <TutorialTitle>Configuração do Usuário</TutorialTitle>
+            <TutorialStep title="Alterar dados">
+              <p>
+                Clicando em “Vizualizar Perfil” no ícone mais à direita do menu de navegação será direcionado à página
+                de usuário. Onde poderá alterar seus dados cadastrados.
+              </p>
+            </TutorialStep>
+            <TutorialStep title="Foto de Perfil">
+              <p>
+                Na páigna de usuário, poderá alterar sua foto de perfil carregando uma imagem de sua própria máquina.
+              </p>
+            </TutorialStep>
+          </Tutorial>
+        </section>
+      </main>
     </div>
   );
 };
